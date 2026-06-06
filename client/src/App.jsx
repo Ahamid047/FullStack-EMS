64326aa13e45a387dac431f53363277ac8dc0131
Ahamid047 <ahamid234567890@gmail.com>
2026-06-07 00:56:37 +0530

    Implementing the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Layout } from 'lucide-react'
+import {Toaster} from 'react-hot-toaster'
+import { Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Toaster />
+      <Routes>
+        <Route  path ="/login" element ={<Login/>}/>
+        <Route element = {<Layout/>}>
+          <Route path ="/dashboard" element ={<Dashboard/>}/>
+          <Route path ="/employee" element ={<Employee/>}/>
+          <Route path ="/attandance" element ={<Attandance/>}/>
+          <Route path ="/leave" element ={<Leave/>}/>
+          <Route path ="/pay-slips" element ={<PaySlips/>}/>
+          <Route path ="/print-slips" element ={<PrintSlips/>}/>
+          <Route path ="/settings" element ={<Settings/>}/>  
+        
+        </Route>
+      </Routes>
+    </>
   )
 }
 
